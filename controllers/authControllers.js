1dd39dc3da8b443757eac18cd05d2c65a770e53c
Roman Paramzin <filpoyma@gmail.com>
2022-02-07 13:54:37 +0300

    Update authControllers.js

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -21,7 +21,7 @@ exports.createUserAndSession = async (req, res, next) => {
     });
 console.log('user %s created', user.name);
     // записываем в req.session.user данные (id & name) (создаем сессию)
-  req.session.user = (serializeUser(user); // req.session.user -> id, name
+  req.session.user = serializeUser(user); // req.session.user -> id, name
 //   res.cookie("sid", JSON.stringify(serializeUser(user)));
   } catch (err) {
     console.error('Err message:', err.message);
